9c5f902e517915597db1047d781283e521a34e1e
Mai Lê Bảo Khang <baokhang48812002@gmail.com>
2025-09-13 23:00:25 +0700

    Correction of comments

--- easterdate.py
+++ easterdate.py
@@ -21,7 +21,7 @@ def easter(y, cal = True):
     k = y // 100
     p = (13 + 8 * k) // 25
     q = k // 4
-    if cal: # Việt Nam không dùng lịch Julian nên ta quy ước lấy 1582.
+    if cal: # Việt Nam không dùng lịch Julian nên ta quy ước lấy 15/10/1582.
         m = (15 - p + k - q) % 30 if y > 1582 else 15
         n = (4 + k - q) % 7 if y > 1582 else 6
     else:
@@ -29,7 +29,7 @@ def easter(y, cal = True):
         n = 6
     d = (19 * a + m) % 30
     if d == 29 or (d == 28 and a > 10): 
-        # Xử lý edge case đ/v epact 24 và 25 đen (25 chỉ đen khi a > 10)
+        # Xử lý ngoại lệ đ/v epact 24 và 25 đen (25 chỉ đen khi a > 10)
         d -= 1
     e = (2 * b + 4 * c + 6 * d + n) % 7
     March = d + e + 22
@@ -41,4 +41,4 @@ def easter(y, cal = True):
     return f + timedelta(j)
 
 if __name__ == "__main__": # Ví dụ
-    print(easter(date.today().year)) # Lễ Phục Sinh năm nay
+    print(easter(date.today().year, False)) # Lễ Phục Sinh năm nay
